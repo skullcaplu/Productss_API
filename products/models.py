dc4df33b351407650eb4ebfbe847fb3fae60fe2d
Luis Forbes <lu@Luiss-iMac.local>
2023-02-17 16:52:10 -0500

    updated project urls+ created first view function + created products app view file

--- products/models.py
+++ products/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    make = models.CharField(max_length=255)
-    model = models.CharField(max_length=255)
-    year = models.IntegerField()
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    inventory_quantity = models.IntegerField()
 
-    
